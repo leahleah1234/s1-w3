ec59881ce72da8a5ec773134ca2f101a3110d07a
leah <leahitis@gmail.com>
2026-08-26 12:04:26 -0700

    Candy Jar Done

--- CandyJar.java
+++ CandyJar.java
@@ -1,4 +1,24 @@
-public class CandyJar{
+public class CandyJar {
+    private int amount;
 
-    
+    public CandyJar() {
+        amount = 10;
+    }
+
+    public CandyJar(int a) {
+        amount = a;
+    }
+
+    public void eat(int amt) {
+        amount -= amt;
+    }
+
+    public void refill(int amt) {
+        amount += amt;
+    }
+
+    public void printState() {
+        System.out.println("Candy Amount: " + amount);
+    }
+
 }
---
+++ CandyJarMain.java
@@ -0,0 +1,17 @@
+public class CandyJarMain {
+    public static void main(String[] args) {
+        CandyJar c = new CandyJar();
+        CandyJar c2 = new CandyJar();
+
+        c.eat(4);
+        c2.eat(6);
+
+        c.refill(12);
+        c2.refill(30);
+        System.out.println("C1: ");
+        c.printState();
+        
+        System.out.println("C2 : ");
+        c2.printState();
+    }
+}
